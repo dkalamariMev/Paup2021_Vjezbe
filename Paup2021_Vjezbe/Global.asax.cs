6708fbe430380badf0365e0e57f552f998fe5c63
Dino Kalamari <dino.kalamari@ck.t-com.hr>
2021-05-18 22:06:45 +0200

    Vježba 12

--- Paup2021_Vjezbe/Global.asax.cs
+++ Paup2021_Vjezbe/Global.asax.cs
@@ -21,6 +21,15 @@ namespace Paup2021_Vjezbe
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
 
+        /// <summary>
+        /// Kod svakog korisničkog zahtjeva čitamo podatke o logiranom korisniku
+        /// iz Cookiea, na način da dekriptiramo te podatke, deserijaliziramo ih,
+        /// spremimo u objekt klase LogiraniKorisnik koji onda spremimo u
+        /// HttpContext.Current.User koji nam je potreban za provjeru da li je korisnik autentificiran
+        /// i provjeru da li korisnik ima prava pristupa po razini ovlasti
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         public void Application_PostAuthenticateRequest(object sender, EventArgs e)
         {
             HttpCookie authCookie = Request.Cookies[FormsAuthentication.FormsCookieName];
