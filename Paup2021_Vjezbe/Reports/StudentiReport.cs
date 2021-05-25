b071acd212d8b9d1ec923c1dfb5c073447fd51f9
Dino Kalamari <dino.kalamari@ck.t-com.hr>
2021-05-25 20:49:40 +0200

    Vježba 13

--- Paup2021_Vjezbe/Reports/StudentiReport.cs
+++ Paup2021_Vjezbe/Reports/StudentiReport.cs
@@ -14,8 +14,8 @@ namespace Paup2021_Vjezbe.Reports
     {
         public byte[] Podaci { get; private set; }
 
-        private PdfPCell GenerirajCeliju(string sadrzaj,
-            Font font, BaseColor boja, bool wrap)
+        // metoda vraća formatirani izgled ćelije
+        private PdfPCell GenerirajCeliju(string sadrzaj, Font font, BaseColor boja, bool wrap)
         {
             PdfPCell c1 = new PdfPCell(new Phrase(sadrzaj, font));
             c1.BackgroundColor = boja;
@@ -29,40 +29,32 @@ namespace Paup2021_Vjezbe.Reports
 
         public void ListaStudenata(List<Student> studenti)
         {
-            BaseFont bfontZaglavlje =
-                BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.CP1250, false);
-            BaseFont bfontTekst =
-                BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1250, false);
-            BaseFont bfontPodnozje =
-                BaseFont.CreateFont(BaseFont.TIMES_ITALIC, BaseFont.CP1250, false);
+            //definiranje fontova
+            BaseFont bfontZaglavlje = BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.CP1250, false);
+            BaseFont bfontTekst = BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1250, false);
+            BaseFont bfontPodnozje = BaseFont.CreateFont(BaseFont.TIMES_ITALIC, BaseFont.CP1250, false);
 
-            Font fontZaglavlje = 
-                new Font(bfontZaglavlje, 12, Font.NORMAL, BaseColor.DARK_GRAY);
+            Font fontZaglavlje = new Font(bfontZaglavlje, 12, Font.NORMAL, BaseColor.DARK_GRAY);
+            Font fontZaglavljeBold = new Font(bfontZaglavlje, 12, Font.BOLD, BaseColor.DARK_GRAY);
+            Font fontNaslov = new Font(bfontTekst, 14, Font.BOLDITALIC, BaseColor.DARK_GRAY);
+            Font fontTablicaZaglavlje = new Font(bfontTekst, 10, Font.BOLD, BaseColor.WHITE);
+            Font fontTekst = new Font(bfontTekst, 10, Font.NORMAL, BaseColor.BLACK);
 
-            Font fontZaglavljeBold =
-                new Font(bfontZaglavlje, 12, Font.BOLD, BaseColor.DARK_GRAY);
-
-            Font fontNaslov =
-                new Font(bfontTekst, 14, Font.BOLDITALIC, BaseColor.DARK_GRAY);
-
-            Font fontTablicaZaglavlje =
-                new Font(bfontTekst, 10, Font.BOLD, BaseColor.WHITE);
-
-            Font fontTekst =
-                new Font(bfontTekst, 10, Font.NORMAL, BaseColor.BLACK);
-
+            //plava boja pozadine za header tablice
             BaseColor tPozadinaZaglavlje = new BaseColor(11, 65, 121);
+            //bijela boja pozadine za ćelije sa sadržajem
             BaseColor tPozadinaSadrzaj = BaseColor.WHITE;
 
-            using(MemoryStream mstream = new MemoryStream())
+            using (MemoryStream mstream = new MemoryStream())
             {
-                using (Document pdfDokument
-                    = new Document(PageSize.A4, 50, 50, 20, 50))
+                using (Document pdfDokument = new Document(PageSize.A4, 50, 50, 20, 50))
                 {
                     PdfWriter.GetInstance(pdfDokument, mstream).CloseStream = false;
 
+                    //Otvaranje dokumenta
                     pdfDokument.Open();
 
+                    //Kreiramo tablicu za ispis zaglavlja - 1. kolona logotip, 2. tekst
                     PdfPTable tZaglavlje = new PdfPTable(2);
                     tZaglavlje.HorizontalAlignment = Element.ALIGN_LEFT;
                     tZaglavlje.DefaultCell.Border = Rectangle.NO_BORDER;
@@ -70,17 +62,20 @@ namespace Paup2021_Vjezbe.Reports
                     float[] sirinaKolonaZag = new float[] { 1f, 3f };
                     tZaglavlje.SetWidths(sirinaKolonaZag);
 
-                    var logo = iTextSharp.text.Image.GetInstance(
-                        HostingEnvironment.MapPath("~/Images/MEV_LOGO.jpg"));
+                    //učitavamo sliku
+                    var logo = iTextSharp.text.Image.GetInstance(HostingEnvironment.MapPath("~/Images/MEV_LOGO.jpg"));
                     logo.Alignment = Element.ALIGN_LEFT;
                     logo.ScaleAbsoluteWidth(50);
                     logo.ScaleAbsoluteHeight(50);
+
+                    //kreiramo ćeliju za logotip
                     PdfPCell cLogo = new PdfPCell(logo);
                     cLogo.Border = Rectangle.NO_BORDER;
                     tZaglavlje.AddCell(cLogo);
 
                     Paragraph info = new Paragraph();
                     info.Alignment = Element.ALIGN_RIGHT;
+                    //definiramo prored = 1.2f - 1.2 * veličina fonta
                     info.SetLeading(0, 1.2f);
                     info.Add(new Chunk("MEĐIMURSKO VELEUČILIŠTE U ČAKOVCU \n", fontZaglavljeBold));
                     info.Add(new Chunk("Bana Josipa Jelačića 22a \n Čakovec \n", fontZaglavlje));
@@ -91,55 +86,75 @@ namespace Paup2021_Vjezbe.Reports
                     cInfo.Border = Rectangle.NO_BORDER;
                     tZaglavlje.AddCell(cInfo);
 
+                    //dodajemo tablicu zaglavlja u dokument
                     pdfDokument.Add(tZaglavlje);
 
+                    // naslov
                     Paragraph pNaslov = new Paragraph("POPIS STUDENATA", fontNaslov);
                     pNaslov.Alignment = Element.ALIGN_CENTER;
                     pNaslov.SpacingBefore = 20;
                     pNaslov.SpacingAfter = 20;
                     pdfDokument.Add(pNaslov);
 
-                    PdfPTable t = new PdfPTable(5);
-                    t.WidthPercentage = 100;
-                    t.SetWidths(new float[] { 1, 3, 2, 1, 4 });
+                    // tablica za studente
+                    PdfPTable t = new PdfPTable(5); // 5 kolona
+                    t.WidthPercentage = 100; // širina tablice u odnosu na veličinu dokumenta
+                    t.SetWidths(new float[] { 1, 3, 2, 1, 4 }); // relativni odnosi širina kolona
 
-                    t.AddCell(GenerirajCeliju("R.br.", fontTablicaZaglavlje,
-                        tPozadinaZaglavlje, true));
-                    t.AddCell(GenerirajCeliju("Ime i prezime", fontTablicaZaglavlje,
-                        tPozadinaZaglavlje, true));
-                    t.AddCell(GenerirajCeliju("OIB", fontTablicaZaglavlje,
-                        tPozadinaZaglavlje, true));
-                    t.AddCell(GenerirajCeliju("Redovni", fontTablicaZaglavlje,
-                        tPozadinaZaglavlje, true));
-                    t.AddCell(GenerirajCeliju("Smjer", fontTablicaZaglavlje,
-                        tPozadinaZaglavlje, true));
+                    // definiranje zaglavlja tablice - dodajemo kolone
+                    t.AddCell(GenerirajCeliju("R.br.", fontTablicaZaglavlje, tPozadinaZaglavlje, true));
+                    t.AddCell(GenerirajCeliju("Ime i prezime", fontTablicaZaglavlje, tPozadinaZaglavlje, true));
+                    t.AddCell(GenerirajCeliju("OIB", fontTablicaZaglavlje, tPozadinaZaglavlje, true));
+                    t.AddCell(GenerirajCeliju("Redovni", fontTablicaZaglavlje, tPozadinaZaglavlje, true));
+                    t.AddCell(GenerirajCeliju("Smjer", fontTablicaZaglavlje, tPozadinaZaglavlje, true));
 
-                    int i = 1;
-                    foreach(Student s in studenti)
+                    // dodajemo redove
+                    int i = 1; // brojač redova
+                    foreach (Student s in studenti)
                     {
-                        t.AddCell(GenerirajCeliju(i.ToString(), fontTekst,
-                            tPozadinaSadrzaj, false));
-                        t.AddCell(GenerirajCeliju(s.PrezimeIme, fontTekst,
-                            tPozadinaSadrzaj, false));
-                        t.AddCell(GenerirajCeliju(s.Oib, fontTekst,
-                            tPozadinaSadrzaj, false));
-                        t.AddCell(GenerirajCeliju(s.RedovniStudent ? "DA" : "NE", fontTekst,
-                            tPozadinaSadrzaj, false));
-                        t.AddCell(GenerirajCeliju(s.UpisaniSmjer?.Naziv, fontTekst,
-                            tPozadinaSadrzaj, false));
+                        t.AddCell(GenerirajCeliju(i.ToString(), fontTekst, tPozadinaSadrzaj, false));
+                        t.AddCell(GenerirajCeliju(s.PrezimeIme, fontTekst, tPozadinaSadrzaj, false));
+                        t.AddCell(GenerirajCeliju(s.Oib, fontTekst, tPozadinaSadrzaj, false));
+                        t.AddCell(GenerirajCeliju(s.RedovniStudent ? "DA" : "NE", fontTekst, tPozadinaSadrzaj, false));
+                        t.AddCell(GenerirajCeliju(s.UpisaniSmjer?.Naziv, fontTekst, tPozadinaSadrzaj, false));
                         i++;
                     }
 
+                    // dodajemo tablicu na dokument
                     pdfDokument.Add(t);
 
-                    Paragraph pMjesto = new Paragraph("Čakovec, " +
-                        DateTime.Now.ToString("dd.MM.yyyy"), fontTekst);
+                    // mjesto i vrijeme
+                    Paragraph pMjesto = new Paragraph("Čakovec, " + DateTime.Now.ToString("dd.MM.yyyy"), fontTekst);
                     pMjesto.Alignment = Element.ALIGN_RIGHT;
                     pMjesto.SpacingBefore = 30;
                     pdfDokument.Add(pMjesto);
                 }
 
                 Podaci = mstream.ToArray();
+
+                //otvaramo generirani PDF iz memorije i upisujemo broj stranice u podnožje
+                using (var reader = new PdfReader(Podaci))
+                {
+                    using (var ms = new MemoryStream())
+                    {
+                        using (var stamper = new PdfStamper(reader, ms))
+                        {
+                            int PageCount = reader.NumberOfPages;
+                            for (int i = 1; i <= PageCount; i++)
+                            {
+                                Rectangle pageSize = reader.GetPageSize(i);
+                                PdfContentByte canvas = stamper.GetOverContent(i);
+
+                                canvas.BeginText();
+                                canvas.SetFontAndSize(bfontPodnozje, 10);
+
+                                canvas.ShowTextAligned(PdfContentByte.ALIGN_RIGHT, $"Stranica {i} / {PageCount}", pageSize.Right - 50, 30, 0);
+                                canvas.EndText();
+                            }
+                        }
+                        Podaci = ms.ToArray();
+                    }
+                }
             }
         }
     }
