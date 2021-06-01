873b6adc7be5ea0a366a6648163d10aa293fb751
mev <mev@KATEDRA-19>
2021-06-01 17:44:27 +0200

    Vježba 14

---
+++ Paup2021Vjezbe_UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Paup2021Vjezbe_UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Paup2021Vjezbe_UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ba3dea6c-f6d6-4341-8a0a-ab5ba20105a7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Paup2021Vjezbe_UnitTest/UnitTest1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity.Infrastructure;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Paup2021_Vjezbe.Controllers;
+using Paup2021_Vjezbe.Models;
+
+namespace Paup2021Vjezbe_UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+       private int Zbroji(int a, int b)
+        {
+            return a + b;
+        }
+
+        [TestMethod]
+        public void TestZbrajanje()
+        {
+            //Arrange
+            int x = 10;
+            int y = 5;
+
+            //Act
+            int zbroj = Zbroji(x, y);
+
+            //Assert
+            Assert.AreEqual(15, zbroj);
+        }
+
+        [TestMethod]
+        public void OIB_ValidacijaDuzina11Znakova()
+        {
+            Student s = new Student()
+            {
+                Oib = new string('a', 10)
+            };
+
+            var context = new ValidationContext(s) 
+                { MemberName = nameof(Student.Oib) };
+
+            var results = new List<ValidationResult>();
+            var valid = Validator.TryValidateProperty(s.Oib, context, results);
+
+            Assert.IsFalse(valid);
+            Assert.AreEqual(1, results.Count);
+            Assert.AreEqual("OIB mora biti duljine 11 znakova", results[0].ErrorMessage);
+        }
+
+        [TestMethod]
+        public void OIB_ValidacijaInvalid()
+        {
+            string oib = "1246";
+            bool valid = OIB.CheckOIB(oib);
+            Assert.IsFalse(valid);
+        }
+
+        [TestMethod]
+        public void OIB_ValidacijaValid()
+        {
+            string oib = "50050041051";
+            bool valid = OIB.CheckOIB(oib);
+            Assert.IsTrue(valid);
+        }
+
+        [TestMethod]
+        public void TestStudentiIndexTitle()
+        {
+            StudentiController controller =
+                new StudentiController();                    
+
+            ViewResult result = controller.Index() as ViewResult;
+
+            Assert.AreEqual("Početna stranica o studentima", 
+                result.ViewBag.Title);
+        }
+
+        [TestMethod]
+        public void Smjerovi_DuplikatException()
+        {
+            SmjeroviController contr =
+                new SmjeroviController();
+
+            Smjer testSmjer = new Smjer()
+            {
+                Sifra = "MS",
+                Naziv = "Menadžment sporta"
+            };
+
+            Assert.ThrowsException<DbUpdateException>(() => contr.Create(testSmjer));
+        }
+
+        [TestMethod]
+        public void Kolegij_KreiranjeBrisanje()
+        {
+            BazaDbContext db = new BazaDbContext();
+
+            Kolegij kolegij = new Kolegij()
+            {
+                Id = 0,
+                Naziv = "Kolegij",
+                Ects = 4,
+                NositeljKolegija = "Mirko",
+                Semestar = 4
+            };
+
+            db.PopisKolegija.Add(kolegij);
+            db.SaveChanges();
+
+            db.PopisKolegija.Remove(kolegij);
+            int obrisano = db.SaveChanges();
+
+            Assert.AreEqual(1, obrisano);
+        }
+    }
+}
